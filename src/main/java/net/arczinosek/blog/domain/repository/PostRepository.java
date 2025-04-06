ec155d4e7f67badbd5eddc589c287ae6389b34ac
Boris Grishenko <arczinosek@gmail.com>
2025-04-07 00:40:41 +0200

    Start using H2 database

--- src/main/java/net/arczinosek/blog/domain/repository/PostRepository.java
+++ src/main/java/net/arczinosek/blog/domain/repository/PostRepository.java
@@ -1,11 +1,7 @@
 package net.arczinosek.blog.domain.repository;
 
 import net.arczinosek.blog.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface PostRepository {
-    Post save(Post post);
-
-    List<Post> findAll();
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
--- src/test/java/net/arczinosek/blog/application/handler/CreatePostHandlerTests.java
+++ src/test/java/net/arczinosek/blog/application/handler/CreatePostHandlerTests.java
@@ -2,47 +2,45 @@ package net.arczinosek.blog.application.handler;
 
 import net.arczinosek.blog.domain.entity.Post;
 import net.arczinosek.blog.domain.repository.PostRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.*;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
 
 public class CreatePostHandlerTests {
+    @Mock
+    private PostRepository postRepository;
+
+    private CreatePostHandler handler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        handler = new CreatePostHandler(postRepository);
+    }
+
     @Test
     public void createsAndSavesNewPostEntityWithPublishDateAsCurrentTime() {
-        var postsRepository = new PostRepository() {
-            private final List<Post> posts = new LinkedList<>();
-
-            @Override
-            public Post save(Post post) {
-                var newPost = post.withId(1L);
-                posts.add(newPost);
-
-                return newPost;
-            }
-
-            @Override
-            public List<Post> findAll() {
-                return posts;
-            }
-        };
-
+        // Arrange
         var command = new CreatePostCommand(
             "Posts title",
             "Posts content"
         );
 
-        var handler = new CreatePostHandler(postsRepository);
+        Mockito.when(
+            postRepository.save(Mockito.any(Post.class))
+        ).thenAnswer(invocation -> invocation.getArgument(0));
 
+        // Act
         var post = handler.handle(command);
 
-        assertInstanceOf(Post.class, post);
-        assertSame(command.title(), post.getTitle());
-        assertSame(command.content(), post.getContent());
-        assertNotNull(post.getPublishedAt());
-        assertSame(1L, post.getId());
-        assertEquals(1, postsRepository.findAll().size());
+        // Assert
+        Assertions.assertEquals(command.title(), post.getTitle());
+        Assertions.assertEquals(command.content(), post.getContent());
+        Assertions.assertTrue(post.getPublishedAt().isBefore(LocalDateTime.now()));
+
+        Mockito.verify(postRepository).save(Mockito.any(Post.class));
     }
 }
